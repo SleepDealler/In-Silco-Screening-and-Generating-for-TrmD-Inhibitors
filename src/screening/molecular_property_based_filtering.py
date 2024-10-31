c947ad695504ceaaa09c7eddc4eed66b5ff84e99
pszmk <przemyslawpietrzak2001@gmail.com>
2024-10-31 10:09:50 +0100

    start development of initial screening

---
+++ src/screening/molecular_property_based_filtering.py
@@ -0,0 +1,312 @@
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+from rdkit.Chem import RDConfig
+import sys
+import os
+
+from typing import Dict, Any
+
+sys.path.append(os.path.join(RDConfig.RDContribDir, "SA_Score"))
+import sascorer
+
+sys.path.append(os.path.join(RDConfig.RDContribDir, "NP_Score"))
+import npscorer
+import csv
+from pathlib import Path
+
+
+class MolecularFilter:
+    name: str
+
+    def apply(self, *args, **kwargs) -> bool:
+        raise NotImplementedError("Subclasses should implement this method")
+
+
+class LipinskiRuleOf5(MolecularFilter):
+    name = "Lipinski_Rule_of_5"
+
+    def apply(
+        self,
+        molecular_weight: float,
+        logp: float,
+        h_bond_donor: int,
+        h_bond_acceptors: int,
+        rotatable_bonds: int,
+    ) -> bool:
+        return (
+            molecular_weight <= 500
+            and logp <= 5
+            and h_bond_donor <= 5
+            and h_bond_acceptors <= 5
+            and rotatable_bonds <= 5
+        )
+
+
+class GhoseFilter(MolecularFilter):
+    name = "Ghose_Filter"
+
+    def apply(
+        self,
+        molecular_weight: float,
+        logp: float,
+        number_of_atoms: int,
+        molar_refractivity: float,
+    ) -> bool:
+        return (
+            molecular_weight >= 160
+            and molecular_weight <= 480
+            and logp >= 0.4
+            and logp <= 5.6
+            and number_of_atoms >= 20
+            and number_of_atoms <= 70
+            and molar_refractivity >= 40
+            and molar_refractivity <= 130
+        )
+
+
+class VeberFilter(MolecularFilter):
+    name = "Veber_Filter"
+
+    def apply(
+        self, rotatable_bonds: int, topological_surface_area_mapping: float
+    ) -> bool:
+        return rotatable_bonds <= 10 and topological_surface_area_mapping <= 140
+
+
+class RuleOf3(MolecularFilter):
+    name = "Rule_of_3_Filter"
+
+    def apply(
+        self,
+        molecular_weight: float,
+        logp: float,
+        h_bond_donor: int,
+        h_bond_acceptors: int,
+        rotatable_bonds: int,
+    ) -> bool:
+        return (
+            molecular_weight <= 300
+            and logp <= 3
+            and h_bond_donor <= 3
+            and h_bond_acceptors <= 3
+            and rotatable_bonds <= 3
+        )
+
+
+class ReosFilter(MolecularFilter):
+    name = "REOS_Filter"
+
+    def apply(
+        self,
+        molecular_weight: float,
+        logp: float,
+        h_bond_donor: int,
+        h_bond_acceptors: int,
+        formal_charge: int,
+        rotatable_bonds: int,
+        heavy_atoms: int,
+    ) -> bool:
+        return (
+            molecular_weight >= 200
+            and molecular_weight <= 500
+            and logp >= -5
+            and logp <= 5
+            and h_bond_donor >= 0
+            and h_bond_donor <= 5
+            and h_bond_acceptors >= 0
+            and h_bond_acceptors <= 10
+            and formal_charge >= -2
+            and formal_charge <= 2
+            and rotatable_bonds >= 0
+            and rotatable_bonds <= 8
+            and heavy_atoms >= 15
+            and heavy_atoms <= 50
+        )
+
+
+class DrugLikeFilter(MolecularFilter):
+    name = "Drug_like_Filter"
+
+    def apply(
+        self,
+        molecular_weight: float,
+        num_of_rings: int,
+        rotatable_bonds: int,
+        h_bond_donor: int,
+        h_bond_acceptors: int,
+        logp: float,
+    ) -> bool:
+        return (
+            molecular_weight < 400
+            and num_of_rings > 0
+            and rotatable_bonds < 5
+            and h_bond_donor <= 5
+            and h_bond_acceptors <= 10
+            and logp < 5
+        )
+
+
+class QEDFilter(MolecularFilter):
+    name = "QED_Filter"
+
+    def apply(self, molecule: rdkit.Chem.Mol) -> float:
+        return Chem.QED.qed(molecule)
+
+
+class TPSAFilter(MolecularFilter):
+    name = "TPSA_Filter"
+
+    def apply(self, molecule: rdkit.Chem.Mol) -> float:
+        return Chem.Crippen.MolTPSA(molecule)
+
+
+class SAScoreFilter(MolecularFilter):
+    name = "SA_Score_Filter"
+
+    def apply(self, molecule: rdkit.Chem.Mol) -> float:
+        return sascorer.calculateScore(molecule)
+
+
+class NPScoreFilter(MolecularFilter):
+    name = "NP_Score_Filter"
+
+    def apply(self, molecule: rdkit.Chem.Mol) -> float:
+        return npscorer.scoreMol(molecule)
+
+
+class MolecularPropertyCalculator:
+    def __init__(self, filters_to_include: list[str] = None):
+        all_filters = {
+            LipinskiRuleOf5.name: LipinskiRuleOf5(),
+            GhoseFilter.name: GhoseFilter(),
+            VeberFilter.name: VeberFilter(),
+            RuleOf3.name: RuleOf3(),
+            ReosFilter.name: ReosFilter(),
+            DrugLikeFilter.name: DrugLikeFilter(),
+            QEDFilter.name: QEDFilter(),
+            TPSAFilter.name: TPSAFilter(),
+            SAScoreFilter.name: SAScoreFilter(),
+            NPScoreFilter.name: NPScoreFilter(),
+        }
+        if filters_to_include is None:
+            self.filters = all_filters
+        else:
+            self.filters = {
+                name: all_filters[name]
+                for name in filters_to_include
+                if name in all_filters
+            }
+
+    def calculate_basic_filters(
+        self, molecule: None | rdkit.Chem.Mol
+    ) -> Dict[str, Any]:
+        results: Dict[str, Any] = {name: None for name in self.filters.keys()}
+        results["Errors"] = []
+
+        if molecule is None:
+            results["Errors"].append("Molecule is None")
+            return results
+
+        try:
+            molecular_weight: float = Descriptors.ExactMolWt(molecule)
+            logp: float = Descriptors.MolLogP(molecule)
+            h_bond_donor: int = Descriptors.NumHDonors(molecule)
+            h_bond_acceptors: int = Descriptors.NumHAcceptors(molecule)
+            rotatable_bonds: int = Descriptors.NumRotatableBonds(molecule)
+            number_of_atoms: int = molecule.GetNumAtoms()
+            molar_refractivity: float = Chem.Crippen.MolMR(molecule)
+            topological_surface_area_mapping: float = Chem.QED.properties(molecule).PSA
+            formal_charge: int = Chem.rdmolops.GetFormalCharge(molecule)
+            heavy_atoms: int = molecule.GetNumHeavyAtoms()
+            num_of_rings: int = Chem.rdMolDescriptors.CalcNumRings(molecule)
+        except Exception as e:
+            results["Errors"].append(str(e))
+            return results
+
+        try:
+            if LipinskiRuleOf5.name in self.filters:
+                results[LipinskiRuleOf5.name] = self.filters[
+                    LipinskiRuleOf5.name
+                ].apply(
+                    molecular_weight,
+                    logp,
+                    h_bond_donor,
+                    h_bond_acceptors,
+                    rotatable_bonds,
+                )
+            if GhoseFilter.name in self.filters:
+                results[GhoseFilter.name] = self.filters[GhoseFilter.name].apply(
+                    molecular_weight, logp, number_of_atoms, molar_refractivity
+                )
+            if VeberFilter.name in self.filters:
+                results[VeberFilter.name] = self.filters[VeberFilter.name].apply(
+                    rotatable_bonds, topological_surface_area_mapping
+                )
+            if RuleOf3.name in self.filters:
+                results[RuleOf3.name] = self.filters[RuleOf3.name].apply(
+                    molecular_weight,
+                    logp,
+                    h_bond_donor,
+                    h_bond_acceptors,
+                    rotatable_bonds,
+                )
+            if ReosFilter.name in self.filters:
+                results[ReosFilter.name] = self.filters[ReosFilter.name].apply(
+                    molecular_weight,
+                    logp,
+                    h_bond_donor,
+                    h_bond_acceptors,
+                    formal_charge,
+                    rotatable_bonds,
+                    heavy_atoms,
+                )
+            if DrugLikeFilter.name in self.filters:
+                results[DrugLikeFilter.name] = self.filters[DrugLikeFilter.name].apply(
+                    molecular_weight,
+                    num_of_rings,
+                    rotatable_bonds,
+                    h_bond_donor,
+                    h_bond_acceptors,
+                    logp,
+                )
+            if QEDFilter.name in self.filters:
+                results[QEDFilter.name] = self.filters[QEDFilter.name].apply(molecule)
+            if TPSAFilter.name in self.filters:
+                results[TPSAFilter.name] = self.filters[TPSAFilter.name].apply(molecule)
+            if SAScoreFilter.name in self.filters:
+                results[SAScoreFilter.name] = self.filters[SAScoreFilter.name].apply(
+                    molecule
+                )
+            if NPScoreFilter.name in self.filters:
+                results[NPScoreFilter.name] = self.filters[NPScoreFilter.name].apply(
+                    molecule
+                )
+        except Exception as e:
+            results["Errors"].append(str(e))
+
+        return results
+
+    def get_result_keys(self) -> list:
+        return list(self.filters.keys()) + ["Errors"]
+
+    def compute_filtering_results_and_save_to_csv(
+        self, supplier: Chem.SDMolSupplier, output_csv_path: Path
+    ):
+        with open(output_csv_path, mode="w", newline="") as csv_file:
+            fieldnames = ["ChemblID"] + self.get_result_keys()
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for molecule in supplier:
+                if molecule is None:
+                    continue
+                chembl_id = (
+                    molecule.GetProp("_Name")
+                    if molecule.HasProp("_Name")
+                    else "Unknown"
+                )
+                results = self.calculate_basic_filters(molecule)
+                results["ChemblID"] = chembl_id
+                writer.writerow(results)
---
+++ src/utils/paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_PATH: Path = Path(__file__).parent.parent.parent / "data"
+
+CHEMBL_34_SDF_PATH: Path = DATA_PATH / "chembl_34.sdf"
